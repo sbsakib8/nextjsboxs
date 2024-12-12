91b7be9455894e54f0de42f0063eaf484c7b8668
sakib <sbsakibsarkar8@gmail.com>
2024-12-13 01:42:14 +0600

    state

---
+++ src/app/State.jsx
@@ -0,0 +1,10 @@
+'use client'
+import React from 'react'
+
+function State() {
+  return (
+    <div className=' text-green-600 bg-red-700'>this is state page</div>
+  )
+}
+
+export default State
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
+'use client'
 import Image from "next/image";
-
+import State from "./State";
 export default function Home() {
   return (
   <>
-    <h1 className=" text-green-700 bg-red-600 text-center">
+    <h1 className=" text-green-700 text-center">
       sb sakib sarkar
     </h1>
+    <State/>
   </>
   );
 }
