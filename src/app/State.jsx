0e720bf69917b7de4afdc5c30754c458a356fbca
sakib <sbsakibsarkar8@gmail.com>
2024-12-13 01:46:56 +0600

    usestate

--- src/app/State.jsx
+++ src/app/State.jsx
@@ -1,9 +1,18 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 
 function State() {
+   const[number,setnumber] = useState(0)
+   let add = ()=>{
+    setnumber(number + 1)
+   };
   return (
-    <div className=' text-green-600 bg-red-700'>this is state page</div>
+    <div className=' text-green-600 '>
+
+        <p>count number : {number}</p>
+        <button className=' bg-red-800 py-5 px-10 rounded-2xl ' onClick={add}>add number</button>
+    </div>
+
   )
 }
 
