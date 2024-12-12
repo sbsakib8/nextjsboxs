aad8b61d1d516079490dc949dfca687c9b15feb3
sakib <sbsakibsarkar8@gmail.com>
2024-12-13 02:49:11 +0600

    routes

---
+++ src/app/about/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>this is about page</div>
+  )
+}
+
+export default page
---
+++ src/app/home/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>this is home page</div>
+  )
+}
+
+export default page
